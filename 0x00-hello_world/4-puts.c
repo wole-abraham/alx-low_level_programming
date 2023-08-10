c432fb99bda745f93782e3c11a7802d1efd24a6a
Abraham <wole1702@gmail.com>
2023-08-10 19:09:32 +0100

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#inlude  < stdio.h >
+/**
+ * main - Entry Point
+ * Description: using the main function
+ * puts: print string
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
+
+
