b6fc384277e1dd81713c1ce5368c8f5f15fb228b
Abraham <wole1702@gmail.com>
2023-08-10 19:17:42 +0100

    5-printd

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+/**
+ * main - program to print a strig
+ * Description: prints a string
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
