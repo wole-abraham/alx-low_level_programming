c874ae0cd63ee1ca436c5d4f291c8b1b522659f2
Abraham <wole1702@gmail.com>
2023-08-15 13:47:21 +0100

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - returns absolute value
+ * Description: above
+ * @c: arguement pased
+ * Return: c
+ */
+
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+
+	else if (c < 0)
+	{
+		return (c * -1);
+	}
+
+}
