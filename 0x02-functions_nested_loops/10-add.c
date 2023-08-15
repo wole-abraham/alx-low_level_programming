84452821150b7c6ebaa3461ddb49e31508c0ad52
Abraham <wole1702@gmail.com>
2023-08-15 23:59:01 +0100

    oh_well

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+/**
+ * add - adds two intergers
+ * Description: adds to integers and returns the sum
+ * @a: interger a
+ * @b: integrer b
+ * Return: result
+ */
+
+
+int add(int a, int b)
+
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
+
+
+
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,24 +1,12 @@
 #include <stdio.h>
 #include "main.h"
-/**
- * print_to_98 - checks if  is a alphabet
- * @n: interger
- * Description: above
- * Return: none
- */
-
 void print_to_98(int n)
 {
-	int a;
-	for (a = n; a <= 98; a++)
+	for (n; n<=98; n++)
 	{
-		if ( a != 98)
-			printf("%d, ", a);
-		else
-			printf("%d", a);
-
+		printf("%d, ", n);
 	}
-
+	
 	_putchar('\n');
-
+	return;
 }
