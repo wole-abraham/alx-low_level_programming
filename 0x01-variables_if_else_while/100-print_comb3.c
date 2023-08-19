7685da10ee35454a25a031492d7df7b3b3cbbee0
Abraham <wole1702@gmail.com>
2023-08-19 20:34:42 +0100

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while( a <= 8)
+	{
+	
+		b = a;
+
+		while (b < 9)
+		{
+			b = b + 1;
+			putchar('0' + a);
+			putchar('0'+ b);
+			if (a != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+		a++;
+
+
+
+	}
+	putchar('\n');
+
+}
+		
