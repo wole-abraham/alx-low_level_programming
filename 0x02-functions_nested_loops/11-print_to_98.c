d4be3bdf44555da2d599d590363607c7c64ecbca
Abraham <wole1702@gmail.com>
2023-08-20 09:44:30 +0100

    11-main.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,44 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * print_to_98 - entry point
+ * @n: parameter
+ * Descrition from n to 98
+ * Return: 0
+ */
+
 void print_to_98(int n)
 {
-	for (n; n<=98; n++)
+	if (n == 98)
+	{
+		printf("%d \n", n);
+	}
+
+	else if (n < 98)
 	{
 		printf("%d, ", n);
+		while (n++ != 98)
+		{
+			if (n == 98)
+				printf("%d \n", n);
+			else
+				printf("%d, ", n);
+		}
 	}
-	
-	_putchar('\n');
-	return;
+
+	else if (n > 98)
+	{
+		printf("%d, ", n);
+		while (n-- != 98)
+		{
+			if (n == 98)
+				printf("%d \n", n);
+			else
+				printf("%d, ", n);
+		}
+	}
+
+
+
+
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,23 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 void times_table(void)
 {
-	int a;
+	int a = 0;
 	int b = 0;
 	int c;
 
 
 	while (a <= 9)
 	{
-		a = 1;
+		
 		while(b <= 12)
 		{
 			c = a * b;
-			_putchar('0' + c);
+			printf("%d", c);
 			b++;
+			_putchar('\n');
 
+		
 		}
 
-	a++;
+		_putchar('\n');
+
+		b = 0;
+		a++;
+
 	}
 }
