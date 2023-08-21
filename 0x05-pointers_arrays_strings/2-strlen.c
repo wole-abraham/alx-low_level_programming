c473e3c3d07b139c5130051a35ab91f662172775
Abraham <wole1702@gmail.com>
2023-08-21 22:04:37 +0100

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen - entry point
+ * @s: pointer of char
+ * Description: oh
+ * Return: i
+ */
+int _strlen(char *s)
+{
+
+	int i;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+
+	return (i);
+
+}
