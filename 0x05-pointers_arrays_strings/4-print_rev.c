bb524f72a41067edca97139ebec11c2875a2fe93
Abraham <wole1702@gmail.com>
2023-08-22 01:44:04 +0100

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - rev char
+ * Description: above
+ * Return:
+ * @s: param
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+
+	for (i; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+
+}
