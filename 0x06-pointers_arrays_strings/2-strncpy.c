08b1cc20e8fd6b5f8c593b61571d162ed57ea3cf
abraham <wole1702@gmail.com>
2023-08-25 05:14:58 -0700

    h

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+ * _strncpy - copies char frm @src to @dest
+ * @src: char poinyer
+ * @n : number of char to be copied
+ * @dest: char poinyyer
+ * Description: ^^
+ * Return: pointer to dest
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
