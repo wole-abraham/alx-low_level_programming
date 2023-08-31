be6c6c912fe01fb92f066aacc3a7ddec4db59903
abraham <wole1702@gmail.com>
2023-08-30 18:26:49 -0700

    0x08

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,3 +3,6 @@ char _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
+int find_root(int n, int i);
+int find_multiplier(int n, int i);
