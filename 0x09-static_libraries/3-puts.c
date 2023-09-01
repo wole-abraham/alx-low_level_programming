607dc5665ff7d16a69d5dc38a9f5ecfe817e4243
abraham <wole1702@gmail.com>
2023-09-01 13:57:40 -0700

    create_static_lib.sh

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - entry point
+ * @str: pointer char
+ * Description: i really dont understand str[i] ins *str[i]
+ * Return:
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+
+}
