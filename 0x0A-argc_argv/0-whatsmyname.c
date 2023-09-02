24694e4eab99f6746fcf2211be5960238ff0385d
abraham <wole1702@gmail.com>
2023-09-01 19:59:30 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: always 0
+ * Description: prints the name of the program
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
