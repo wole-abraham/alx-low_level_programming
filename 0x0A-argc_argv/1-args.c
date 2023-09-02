af3905fd501bfdc54c1de531474be6a695372c6f
abraham <wole1702@gmail.com>
2023-09-01 20:21:20 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: arguement counter
+ * @argv: arguement vector
+ * Description: ^^^^
+ * Return: always 0
+ */
+
+int main(int argc, char *argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+
+	}
+
+	i = i - 1;
+	printf("%d\n", i);
+
+	return (0);
+}
