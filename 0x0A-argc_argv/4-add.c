b297858e16d8cea02108e7598cc83a64b2a9ff72
abraham <wole1702@gmail.com>
2023-09-02 06:54:55 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,45 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+/**
+ * main - entry poin
+ * @argc: arguement counter
+ * @argv: arguement vecot
+ * Description: adds h
+ * Return: 0 or 1
+ */
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int c, d;
-	int sum = 0;
+	int i, c, d, sum;
 
-	if (argc <= 1)
+	if (argc == 0)
 	{
 		printf("%d\n", 0);
 		return (0);
+
 	}
 
+
 	for (c = 1; c < argc; c++)
-	{
 		for (d = 0; argv[c][d] != '\0'; d++)
-		{
 			if (argv[c][d] < '0' || argv[c][d] > '9')
 			{
 				printf("Error\n");
 				return (1);
+
 			}
-		}
-
-	}
-
-
-
+	sum = 0;
 	for (i = 1; i < argc; i++)
 		sum = sum + atoi(argv[i]);
-
-	printf("%d", sum);
+	printf("%d\n", sum);
 
 	return (0);
 
 }
-
-
-
 
 
 
