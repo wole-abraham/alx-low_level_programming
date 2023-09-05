1d6cc652f2288146e5e6645f881366a54a4de195
abraham <wole1702@gmail.com>
2023-09-05 06:38:31 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+/**
+ * str_concat - Entry poinnt
+ * @s1: strig fi
+ * @s2: string nd
+ * Description: ^^^^^^^^^^^
+ * Return: NULL or s
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+
+	unsigned int i, j;
+
+	s = malloc((sizeof(s1) + sizeof(s2) - 1) * sizeof(char));
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (s);
+	}
+
+
+	for (i = 0; s1[i] != '\0'; i++)
+		s[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		s[i] = s2[j];
+		i++;
+	}
+
+	s[i] = '\0';
+
+	return (s);
+
+}
+
