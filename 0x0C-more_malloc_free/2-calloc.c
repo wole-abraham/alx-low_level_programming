78051c5fc8f01774e3b7f81d9ded6e0b5c30a5fe
abraham <wole1702@gmail.com>
2023-09-08 12:46:25 -0700

    added

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,7 +3,7 @@
 /**
  * _calloc - Allocate memory for array of nmemb elements of size bytes
  * @nmemb: Number of elemnts
- * @size: Size in bytes of elements 
+ * @size: Size in bytes of elements
  * Return: Pointer to new memory, NULL if it fails
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,7 +3,7 @@
 /**
  * array_range - Create array of integers filled with numbers from min to max
  * @min: First number in the array
- * @max: Largest number in the array 
+ * @max: Largest number in the array
  * Return: Pointer to new array, NULL if failed
  */
 int *array_range(int min, int max)
