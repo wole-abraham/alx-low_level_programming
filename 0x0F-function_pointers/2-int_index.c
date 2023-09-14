2b88330dcfed34efe1fb72eb5f66c5084554efc6
abraham <wole1702@gmail.com>
2023-09-13 18:18:05 -0700

    added

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+/**
+ * int_index - Entry poiny
+ * @array: array
+ * @size: size
+ * @cmp: pointer to funvyio
+ * Return: -1 or
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		exit(1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+
+	}
+
+	return (-1);
+
+}
+
+
+
